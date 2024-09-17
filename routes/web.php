79460ee8f9295176ad9a91f9f87d16427ec6dda0
SOPORTE WEB SSC <soportewebssc@gnmail.com>
2024-09-17 14:26:34 -0600

    SE PROTEGIERON RUTAS

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@ use App\Http\Controllers\SitioController;
 use App\Http\Controllers\UnidadController;
 use App\Http\Controllers\UserController;
 
-Auth::routes();
-
 /**
  * 
  * 
@@ -44,8 +42,128 @@ Route::get('/incidentes/opciones/{categoria_id}', [SitioController::class, 'getO
  * 
  */
 
-// Mostramos las graficas y el Dashboard
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Auth::routes([
+    ['register' => false]
+]);
+
+// Rutas protegidas por autenticación
+Route::middleware('auth')->group(function () {
+
+    /**
+     * 
+     * 
+     * DASHBOARD
+     * 
+     * 
+     */
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    /**
+     * 
+     * 
+     * MODULO DE TIMBRADO
+     * 
+     * 
+     */
+
+    //Ruta para mostrar todos los registros
+    Route::get('admin/timbradoIndex',[CorreoController::class,'index'])->name('timbradoIndex');
+
+    //Ruta para mostrar el formulario de creacion de registro
+    Route::get('admin/timbradoCreate',[CorreoController::class,'create'])->name('timbradoCreate');
+
+    //Ruta para almacenar un registro en la DB
+    Route::post('admin/timbradoStore',[CorreoController::class,'store'])->name('timbradoStore');
+
+    //Ruta para mostrar el formulario de edicion de registro
+    Route::get('admin/timbradoEdit/{id}', [CorreoController::class, 'edit'])->name('timbradoEdit');
+
+    //Ruta para actualizar los campos en la DB
+    Route::put('admin/timbradoUpdate/{id}',[CorreoController::class,'update'])->name('timbradoUpdate');
+
+    //Ruta para eliminar un registro de la DB
+    Route::get('admin/timbradoDestroy/{id}',[CorreoController::class,'destroy'])->name('timbradoDestroy');
+
+    /**
+     * 
+     * 
+     * MODULO DE UNIDADES
+     * 
+     * 
+     */
+
+    //Ruta para mostra la lista de las unidades
+    Route::get('admin/unidadIndex',[UnidadController::class,'index'])->name('unidadIndex');
+
+    //Ruta para mostrar el formulario de creacion de registros
+    Route::get('admin/unidadCreate',[UnidadController::class,'create'])->name('unidadCreate');
+
+    //Ruta para almacenar el registro en la DB
+    Route::post('admin/unidadStore',[UnidadController::class,'store'])->name('unidadStore');
+
+    //Ruta para mostrar el formulario de edicion de unidad
+    Route::get('admin/unidadEdit/{id}',[UnidadController::class,'edit'])->name('unidadEdit');
+
+    //Ruta para actualizar los campos en la DB
+    Route::put('admin/unidadUpdate/{id}',[UnidadController::class,'update'])->name('unidadUpdate');
+
+    //Ruta oara eliminar un registro de la DB
+    Route::get('admin/unidadDestroy/{id}',[UnidadController::class,'destroy'])->name('unidadDestroy');
+
+    /**
+     * 
+     * 
+     * MODULO DE USUARIOS
+     * 
+     * 
+     */
+
+    // Ruta para mostrar la lista de usuarios
+    Route::get('admin/usuarioIndex',[UserController::class,'index'])->name('usuarioIndex');
+
+    // Ruta para mostrar el formulario de creacion de registros
+    Route::get('admin/usuarioCreate',[UserController::class,'create'])->name('usuarioCreate');
+
+    // Ruta para guardar el registro en la db
+    Route::post('admin/usuarioStore',[UserController::class,'store'])->name('usuarioStore');
+
+    /**
+    * 
+    *
+    * MODULO DE EVENTOS
+    *
+    *
+    */
+
+    // Ruta para mostra todos los eventos en la base de datos ADMINISTRADOR
+    Route::get('admin/eventoIndex',[EventoController::class,'index'])->name('eventoIndex');
+
+    // Ruta para mostrar los detalles del evento
+    Route::get('admin/eventoShow/{id}',[EventoController::class,'show'])->name('eventoShow');
+
+    // Ruta para mostrar todos los eventos en la base de datos JURISDICCION
+
+    Route::get('admin/eventoJurisdiccion',[EventoController::class,'jurisdiccion'])->name('eventoJurisdiccion');
+
+     /**
+     * 
+     *
+     * MODULO DE REPORTES
+     *
+     *
+     */
+
+    // Ruta para mostrar el formulario de fechas de reporte
+    Route::get('admin/reporteIndex',[ReporteController::class,'index'])->name('reporteIndex');
+
+    // Ruta para buscar los datos en la tabla
+    Route::post('admin/reporteSearch',[ReporteController::class,'search'])->name('reporteSearch');
+
+    // Ruta para mostrar los resultados
+    Route::get('admin/reporteShow',[ReporteController::class,'show'])->name('reporteShow');
+
+});
 
 
 
@@ -55,107 +173,14 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
 
-/**
- * 
- * 
- * MODULO DE TIMBRADO
- * 
- * 
- */
-
- //Ruta para mostrar todos los registros
-Route::get('admin/timbradoIndex',[CorreoController::class,'index'])->name('timbradoIndex');
-
-//Ruta para mostrar el formulario de creacion de registro
-Route::get('admin/timbradoCreate',[CorreoController::class,'create'])->name('timbradoCreate');
-
-//Ruta para almacenar un registro en la DB
-Route::post('admin/timbradoStore',[CorreoController::class,'store'])->name('timbradoStore');
-
-//Ruta para mostrar el formulario de edicion de registro
-Route::get('admin/timbradoEdit/{id}', [CorreoController::class, 'edit'])->name('timbradoEdit');
-
-//Ruta para actualizar los campos en la DB
-Route::put('admin/timbradoUpdate/{id}',[CorreoController::class,'update'])->name('timbradoUpdate');
-
-//Ruta para eliminar un registro de la DB
-Route::get('admin/timbradoDestroy/{id}',[CorreoController::class,'destroy'])->name('timbradoDestroy');
-
-/**
- * 
- * 
- * MODULO DE UNIDADES
- * 
- * 
- */
-
- //Ruta para mostra la lista de las unidades
- Route::get('admin/unidadIndex',[UnidadController::class,'index'])->name('unidadIndex');
-
- //Ruta para mostrar el formulario de creacion de registros
- Route::get('admin/unidadCreate',[UnidadController::class,'create'])->name('unidadCreate');
-
- //Ruta para almacenar el registro en la DB
- Route::post('admin/unidadStore',[UnidadController::class,'store'])->name('unidadStore');
-
- //Ruta para mostrar el formulario de edicion de unidad
- Route::get('admin/unidadEdit/{id}',[UnidadController::class,'edit'])->name('unidadEdit');
-
- //Ruta para actualizar los campos en la DB
- Route::put('admin/unidadUpdate/{id}',[UnidadController::class,'update'])->name('unidadUpdate');
-
- //Ruta oara eliminar un registro de la DB
- Route::get('admin/unidadDestroy/{id}',[UnidadController::class,'destroy'])->name('unidadDestroy');
-
-/**
- * 
- * 
- * MODULO DE USUARIOS
- * 
- * 
- */
-
- // Ruta para mostrar la lista de usuarios
- Route::get('admin/usuarioIndex',[UserController::class,'index'])->name('usuarioIndex');
-
- // Ruta para mostrar el formulario de creacion de registros
- Route::get('admin/usuarioCreate',[UserController::class,'create'])->name('usuarioCreate');
-
- // Ruta para guardar el registro en la db
- Route::post('admin/usuarioStore',[UserController::class,'store'])->name('usuarioStore');
-
- /**
-  * 
-  *
-  * MODULO DE EVENTOS
-  *
-  *
-  */
-
- // Ruta para mostra todos los eventos en la base de datos ADMINISTRADOR
- Route::get('admin/eventoIndex',[EventoController::class,'index'])->name('eventoIndex');
-
- // Ruta para mostrar los detalles del evento
- Route::get('admin/eventoShow/{id}',[EventoController::class,'show'])->name('eventoShow');
-
- // Ruta para mostrar todos los eventos en la base de datos JURISDICCION
-
- Route::get('admin/eventoJurisdiccion',[EventoController::class,'jurisdiccion'])->name('eventoJurisdiccion');
 
 
- /**
- * 
- *
- * MODULO DE REPORTES
- *
- *
- */
 
- // Ruta para mostrar el formulario de fechas de reporte
- Route::get('admin/reporteIndex',[ReporteController::class,'index'])->name('reporteIndex');
 
- // Ruta para buscar los datos en la tabla
- Route::post('admin/reporteSearch',[ReporteController::class,'search'])->name('reporteSearch');
 
- // Ruta para mostrar los resultados
- Route::get('admin/reporteShow',[ReporteController::class,'show'])->name('reporteShow');
+
+
+
+ 
+
+
