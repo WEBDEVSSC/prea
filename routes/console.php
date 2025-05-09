f6de8c1a491001e4d24034d5c7a8fc9808b91b79
WEB DEV SSC <soportewebssc@gmail.com>
2025-05-09 09:15:44 -0600

    TAREA PROGRAMADA EL DIA 9 A LAS 920

--- routes/console.php
+++ routes/console.php
@@ -11,4 +11,4 @@ Artisan::command('inspire', function () {
 
 // PROGRAMAR EL ENVIO DEL REPORTE MENSUAL CADA DIA 1 DE MES A LAS 8 AM
 Schedule::command('reporte:ejecutar')
-    ->monthlyOn(9, '09:30');
+    ->monthlyOn(9, '09:20');
