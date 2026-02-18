73632eba0b1a3610356b52ecb12b336dcfb02204
WEBDEVSSC <soportewebssc@gmail.com>
2026-02-18 14:04:02 -0600

    FIX | PROVIDERS EN BOOSTRAP APP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withProviders([
+        \Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
+    ])->create();
