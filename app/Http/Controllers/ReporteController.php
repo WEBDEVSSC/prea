5e9d043b14c9f99066829c1423646c1361a82b4d
WEBDEVSSC <soportewebssc@gmail.com>
2026-06-04 19:14:56 -0600

    🐛 FIX | SE AJUSTARON LAS FECHAS EN EL REPORTE MENSUAL A INICIO MES ANTERIOR Y FIN MES ANTERIOR

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -104,11 +104,11 @@ class ReporteController extends Controller
             Storage::makeDirectory('public/pdfs');
         }
 
-        //$inicioMesAnterior = Carbon::now()->subMonthNoOverflow()->startOfMonth()->startOfDay(); // 1er día del mes anterior a las 00:00:00
-        //$finMesAnterior = Carbon::now()->subMonthNoOverflow()->endOfMonth()->endOfDay(); // último día del mes anterior a las 23:59:59
+        $inicioMesAnterior = Carbon::now()->subMonthNoOverflow()->startOfMonth()->startOfDay(); // 1er día del mes anterior a las 00:00:00
+        $finMesAnterior = Carbon::now()->subMonthNoOverflow()->endOfMonth()->endOfDay(); // último día del mes anterior a las 23:59:59
 
-        $inicioMesAnterior = Carbon::create(2026, 1, 1)->startOfDay();  // 1er día del mes anterior a las 00:00:00
-        $finMesAnterior = Carbon::create(2026, 4, 30)->endOfDay(); // último día del mes anterior a las 23:59:59
+        //$inicioMesAnterior = Carbon::create(2026, 1, 1)->startOfDay();  // 1er día del mes anterior a las 00:00:00
+        //$finMesAnterior = Carbon::create(2026, 4, 30)->endOfDay(); // último día del mes anterior a las 23:59:59
 
         // Formateo para mostrar en el PDF
         $inicioMesStr = $inicioMesAnterior->format('d-m-Y');
